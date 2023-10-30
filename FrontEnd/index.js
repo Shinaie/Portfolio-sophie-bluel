bc826b5c8a2bec4ea689872d9e333b5cab7d8732
James Brossard <shinaie@hotmail.fr>
2023-10-30 20:22:25 +0100

    p6

---
+++ FrontEnd/index.js
@@ -0,0 +1,110 @@
+//****************************  Variables globales pour les données des travaux et d es catégories **********************//
+
+let worksData = [];
+let categoriesData = [];
+
+//****************************  Fonction pour récupérer les données des travaux de l'API **********************//
+
+const fetchWorks = async () => {
+  await fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then((data) => {
+      worksData = data;
+      worksDisplay(worksData);
+    });
+};
+fetchWorks();
+
+//****************************  Fonction pour afficher les travaux dans la galerie **********************//
+
+const worksDisplay = (worksData) => {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = worksData
+    .map(
+      (work) =>
+        `<figure>
+           <img src="${work.imageUrl}" alt="photo de ${work.title}">
+           <figcaption>${work.title}</figcaption>
+         </figure>`
+    )
+    .join(""); // J'enlève les virgules
+};
+
+//****************************  Fonction pour récupérer les données des catégories depuis l'API **********************//
+
+const fetchCategories = async () => {
+  await fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((data) => {
+      buttons(data);
+      categoriesData = data;
+    });
+};
+fetchCategories();
+
+//****************************  Fonction pour créer des boutons de filtre en fonction des catégories **********************//
+
+const buttons = (categoriesData) => {
+  const containerFilter = document.getElementById("container-filter");
+
+  //****************************  Bouton "Tous" pour afficher tous les travaux **********************//
+
+  const buttonAll = document.createElement("button");
+  buttonAll.className = "btn";
+  buttonAll.dataset.id = "0";
+  buttonAll.textContent = "Tous";
+  buttonAll.addEventListener("click", () => {
+    filterWorks("Tous");
+  });
+  containerFilter.appendChild(buttonAll);
+
+  //****************************  Création des boutons pour chaque catégorie **********************//
+
+  categoriesData.forEach((category) => {
+    const bouton = document.createElement("button");
+    bouton.classList.add("btn");
+    bouton.textContent = category.name;
+    bouton.addEventListener("click", () => {
+      filterWorks(category.name);
+    });
+    containerFilter.appendChild(bouton);
+  });
+};
+
+//****************************  Fonction pour filtrer les travaux en fonction de la catégorie sélectionnée **********************//
+
+const filterWorks = (categoryName) => {
+  const worksFiltre =
+    categoryName === "Tous"
+      ? worksData
+      : worksData.filter((work) => categoryName === work.category.name);
+
+  worksDisplay(worksFiltre);
+};
+
+//****************************  Mode administrateur **********************//
+
+const tokenAdmin = localStorage.getItem("token");
+const administrator = document.querySelector(".js__modal");
+const loginButton = document.getElementById("loginButton");
+const blackBar = document.querySelector(".blackBar");
+
+//****************************  Vérification de l'état de connexion et gestion du mode administrateur **********************//
+
+const stateLoginButton = function () {
+  if (tokenAdmin) {
+    // l'utilisateur est connecté en mode administrateur
+    loginButton.textContent = "logout";
+    administrator.style.display = null;
+    blackBar.style.display = null;
+    loginButton.addEventListener("click", () => {
+      localStorage.removeItem("token");
+      alert("Vous avez été déconnecté.");
+      stateLoginButton();
+    });
+  } else {
+    loginButton.textContent = "login";
+    administrator.style.display = "none";
+  }
+};
+stateLoginButton();
---
+++ FrontEnd/modal.js
@@ -0,0 +1,276 @@
+// Déclaration des variables globales
+
+let modal = null;
+let modal2 = null;
+
+const token = localStorage.getItem("token");
+const titleInput = document.getElementById("input-title");
+const categorySelect = document.getElementById("category");
+const imageInput = document.getElementById("add__picture__modal");
+const buttonValid = document.getElementById("add__modal");
+
+const button = document.getElementById("add__picture__upload");
+const input = document.querySelector(".add__picture__btn");
+
+//***************************  Fonction pour ouvrir le modal1 ***************//
+
+const openModal = function (e) {
+  e.preventDefault();
+  //target sur une ancre
+  const target = document.querySelector(e.target.getAttribute("href"));
+  target.style.display = null;
+  target.removeAttribute("aria-hidden");
+  target.setAttribute("arial-modal", "true");
+
+  // affiche la gallerie qui est dans le swagger
+
+  const figure = document.querySelector(".gallery__modal");
+  figure.innerHTML = worksData
+    .map(
+      (project) => `
+        <figure>
+          <i class="fa-regular fa-trash-can" id="delete__picture"></i>
+           <img src="${project.imageUrl}" alt="photo de ${project.title}" data-id="${project.id}" >
+        </figure>`
+    )
+    .join("");
+
+  //****************************  Supprimer l'image du modal **********************//
+
+  const deletePictures = document.querySelectorAll("#delete__picture");
+
+  // recherche de l'id de data de chaque images au click
+  deletePictures.forEach((deletePicture) => {
+    deletePicture.addEventListener("click", function () {
+      // supprime l'element parent du bouton
+      const imageParent = deletePicture.parentElement;
+      // obtenir l'id de l'image
+      const pictureData = imageParent
+        .querySelector("img")
+        .getAttribute("data-id");
+      //supprimer l'element parent de figure
+      imageParent.remove();
+      console.log("Image " + pictureData + " supprimée");
+      // suprime l'image ainsi que l'api au click
+      // deletePictureApi(pictureData);
+      // Supprime l'image de la galerie existante
+      const updatedWorksData = worksData.filter(
+        (work) => work.id !== parseInt(pictureData)
+      );
+
+      // Actualise la galerie
+      worksDisplay(updatedWorksData);
+    });
+  });
+
+  modal = target;
+  modal.addEventListener("click", closeModal);
+  modal.querySelector(".close__icon").addEventListener("click", closeModal);
+  modal
+    .querySelector(".js__modal__stop")
+    .addEventListener("click", stopPropagation);
+};
+
+//**************************** Fonction pour supprimer une image de l'api ******************* */
+
+const deletePictureApi = async (pictureData) => {
+  try {
+    // Envoi une requête Delete pour supprimer l'image
+    const response = await fetch(
+      `http://localhost:5678/api/works/${pictureData}`,
+      {
+        method: "DELETE",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    if (response.ok) {
+      console.log("Image " + pictureData + " supprimée de l'Api");
+    } else {
+      console.error("Erreur lors de la suppression de l'image de l'API");
+    }
+  } catch (error) {
+    console.error("Erreur lors de la requête DELETE :" + error);
+  }
+};
+
+//****************************  Fonction pour ajouter un nouveau modal **********************//
+
+const addNewModal = function () {
+  const target2 = document.querySelector("#submit__add__picture");
+  target2.addEventListener("click", (e) => {
+    e.preventDefault();
+    const openModal2 = document.getElementById("modal2");
+    openModal2.style.display = null;
+    modal.style.display = "none";
+    modal2 = openModal2;
+    modal2.addEventListener("click", closeModal2);
+    modal2.querySelector(".close__icon").addEventListener("click", closeModal2);
+    modal2
+      .querySelector(".js__modal__stop")
+      .addEventListener("click", stopPropagation);
+  });
+};
+addNewModal();
+
+//****************************  Fonction pour retourner au modal précédent **********************//
+
+const returnModal = function () {
+  const returnModal1 = document.querySelector(".return__icon");
+  returnModal1.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal2.style.display = "none";
+    modal.style.display = null;
+    resetModal2();
+  });
+};
+returnModal();
+
+//****************************  Fonction Pour afficher charger l'image **********************//
+
+// //on simule un click sur le champ input file
+button.addEventListener("click", () => {
+  input.click();
+});
+
+input.addEventListener("change", function (e) {
+  //on recupere le fichier selectionné du champ
+  let file = e.target.files[0];
+  // traitement et affichage du fichier
+  showFile(file);
+});
+
+//****************************  Fonction Pour afficher le fichier uploadé **********************//
+
+function showFile(file) {
+  let image = document.getElementById("image");
+  document.getElementById("erase").style.display = "none";
+  image.style.display = "block";
+  // on recupère le type du fichier
+  let fileType = file.type;
+  let fileExtension = ["image/jpeg", "image/jpg", "image/png"];
+  //  on verifie la validité du type du fichier
+  if (fileExtension.includes(fileType)) {
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+
+    fileReader.onload = () => {
+      let fileUrl = fileReader.result;
+      image.src = fileUrl;
+    };
+  } else {
+    alert("Ceci n'est pas une image");
+  }
+}
+
+//****************************  Fonction pour changer la couleur du bouton Valider **********************//
+
+const changeBtnColor = function () {
+  if (
+    titleInput.value !== "" &&
+    categorySelect.value !== "0" &&
+    imageInput.files[0] !== undefined
+  ) {
+    buttonValid.style.background = "#1D6154";
+  } else {
+    buttonValid.style.background = "#A7A7A7";
+  }
+};
+
+titleInput.addEventListener("change", changeBtnColor);
+categorySelect.addEventListener("change", changeBtnColor);
+imageInput.addEventListener("change", changeBtnColor);
+
+//****************************  Fonction pour envoyer le formulaire **********************//
+
+buttonValid.addEventListener("click", async (e) => {
+  //Recuperation des champs
+  const title = titleInput.value;
+  const category = categorySelect.value;
+  const imageFile = imageInput.files[0];
+
+  if (title && category && imageFile) {
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("image", imageFile);
+    formData.append("category", category);
+
+    try {
+      const response = await fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+
+      if (response.ok) {
+        let newWorks = await response.json();
+        newWorks.categoryId = parseInt(newWorks.categoryId);
+        worksData.push(newWorks);
+        worksDisplay(worksData);
+        modal2.style.display = "none";
+        modal2 = null;
+        console.log("image envoyée avec succès");
+        resetModal2();
+      } else {
+        console.error("erreur lors de l'envoi");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête à l'API : " + error);
+    }
+  } else {
+    alert("Veuillez remplir tous les champs du formulaire.");
+  }
+});
+
+//****************************  Fonction pour fermer le modal **********************//
+
+const closeModal = function (e) {
+  if (modal === null) return;
+  e.preventDefault();
+  modal.style.display = "none";
+  modal.removeEventListener("click", closeModal);
+  modal.querySelector(".close__icon").removeEventListener("click", closeModal);
+  modal
+    .querySelector(".js__modal__stop")
+    .removeEventListener("click", stopPropagation);
+  modal = null;
+};
+
+const closeModal2 = function (e) {
+  if (modal2 === null) return;
+  e.preventDefault();
+  modal2.style.display = "none";
+  modal2 = null;
+  resetModal2();
+};
+
+//****************************  Fonction pour réinitialiser le modal2 **********************//
+
+const resetModal2 = async function () {
+  titleInput.value = "";
+  categorySelect.value = "0";
+  imageInput.value = "";
+  document.getElementById("erase").style.display = "block";
+  let image = document.getElementById("image");
+  image.style.display = "none";
+};
+
+//****************************  Fonction pour empecher la propagation des événements**********************//
+
+const stopPropagation = function (e) {
+  e.stopPropagation();
+};
+
+document.querySelectorAll(".js__modal").forEach((a) => {
+  a.addEventListener("click", openModal);
+});
+
+//touche echap pour fermer le modal
+
+window.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" || e.key === "Esc") {
+    closeModal(e);
+    closeModal2(e);
+  }
+});
